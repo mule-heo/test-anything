f900c07bd008cbe5ffc8c1dd314700814d8ce7bf
mule-heo <zaq98114@gmail.com>
2023-06-11 22:54:22 +0900

    TEST-6 input page

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 
 function Main() {
   return (
-    <>
-      <header>Header</header>
+    <div>
+      <header>
+        <h1>Reservation</h1>
+      </header>
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 
